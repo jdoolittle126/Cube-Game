8fc139d9de02e1ea6723594a0c8edd1fe7d87042
jdoolittle126 <jdoolittle126@gmail.com>
2019-11-10 15:52:56 -0800

    Revert "Some Formatting"
    This reverts commit 0c0a5fe25d10ff6933efd585a0adcdbb3ebafd60.

--- src/Core/Model.cpp
+++ src/Core/Model.cpp
@@ -1,12 +1,14 @@
 #include "Model.h"
 #include "../Utils/OBJ_Loader.h"
 
-void Model::create_obj_buffer(std::string path) {
+void Model::create_obj_buffer(std::string path)
+{
 	objl::Loader Loader;
 	bool loadout = Loader.LoadFile(path);
 	int size = 0;
 
-	for (unsigned int i = 0; i < Loader.LoadedMeshes.size(); i++) {
+	for (unsigned int i = 0; i < Loader.LoadedMeshes.size(); i++)
+	{
 		objl::Mesh curMesh = Loader.LoadedMeshes[i];
 
 		for (unsigned int a = 0; a < curMesh.Indices.size(); a++) {
@@ -16,30 +18,24 @@ void Model::create_obj_buffer(std::string path) {
 		size += curMesh.Vertices.size();
 
 		for (unsigned int j = 0; j < curMesh.Vertices.size(); j++) {
-			model_verts.push_back(
-					glm::vec3(curMesh.Vertices[j].Position.X,
-							curMesh.Vertices[j].Position.Y,
-							curMesh.Vertices[j].Position.Z));
-			model_uvs.push_back(
-					glm::vec2(curMesh.Vertices[j].TextureCoordinate.X,
-							curMesh.Vertices[j].TextureCoordinate.Y));
-			model_normals.push_back(
-					glm::vec3(curMesh.Vertices[j].Normal.X,
-							curMesh.Vertices[j].Normal.Y,
-							curMesh.Vertices[j].Normal.Z));
+			model_verts.push_back(glm::vec3(curMesh.Vertices[j].Position.X, curMesh.Vertices[j].Position.Y, curMesh.Vertices[j].Position.Z));
+			model_uvs.push_back(glm::vec2(curMesh.Vertices[j].TextureCoordinate.X, curMesh.Vertices[j].TextureCoordinate.Y));
+			model_normals.push_back(glm::vec3(curMesh.Vertices[j].Normal.X, curMesh.Vertices[j].Normal.Y, curMesh.Vertices[j].Normal.Z));
 		}
+
 
 	}
 
-	if (loadout) {
+	if(loadout) {
 		std::cout << "Success" << std::endl;
 	} else {
 		std::cout << "Fail" << std::endl;
 	}
 
+
 }
 
-GLuint Model::build_texture(std::string path) {
+GLuint Model::build_texture(std::string path){
 
 	// Data read from the header of the BMP file
 	unsigned char header[54];
@@ -47,11 +43,11 @@ GLuint Model::build_texture(std::string path) {
 	unsigned int imageSize;
 	unsigned int width, height;
 	// Actual RGB data
-	unsigned char *data;
+	unsigned char * data;
 
 	// Open the file
-	FILE *file = fopen(path.c_str(), "rb");
-	if (!file) {
+	FILE * file = fopen(path.c_str(),"rb");
+	if (!file){
 		getchar();
 		return 0;
 	}
@@ -59,47 +55,39 @@ GLuint Model::build_texture(std::string path) {
 	// Read the header, i.e. the 54 first bytes
 
 	// If less than 54 bytes are read, problem
-	if (fread(header, 1, 54, file) != 54) {
+	if ( fread(header, 1, 54, file)!=54 ){
 		printf("Not a correct BMP file\n");
 		fclose(file);
 		return 0;
 	}
 	// A BMP files always begins with "BM"
-	if (header[0] != 'B' || header[1] != 'M') {
+	if ( header[0]!='B' || header[1]!='M' ){
 		printf("Not a correct BMP file\n");
 		fclose(file);
 		return 0;
 	}
 	// Make sure this is a 24bpp file
-	if (*(int*) &(header[0x1E]) != 0) {
-		printf("Not a correct BMP file\n");
-		fclose(file);
-		return 0;
-	}
-	if (*(int*) &(header[0x1C]) != 24) {
-		printf("Not a correct BMP file\n");
-		fclose(file);
-		return 0;
-	}
+	if ( *(int*)&(header[0x1E])!=0  )         {printf("Not a correct BMP file\n");    fclose(file); return 0;}
+	if ( *(int*)&(header[0x1C])!=24 )         {printf("Not a correct BMP file\n");    fclose(file); return 0;}
 
 	// Read the information about the image
-	dataPos = *(int*) &(header[0x0A]);
-	imageSize = *(int*) &(header[0x22]);
-	width = *(int*) &(header[0x12]);
-	height = *(int*) &(header[0x16]);
+	dataPos    = *(int*)&(header[0x0A]);
+	imageSize  = *(int*)&(header[0x22]);
+	width      = *(int*)&(header[0x12]);
+	height     = *(int*)&(header[0x16]);
 
 	// Some BMP files are misformatted, guess missing information
-	if (imageSize == 0) imageSize = width * height * 3; // 3 : one byte for each Red, Green and Blue component
-	if (dataPos == 0) dataPos = 54; // The BMP header is done that way
+	if (imageSize==0)    imageSize=width*height*3; // 3 : one byte for each Red, Green and Blue component
+	if (dataPos==0)      dataPos=54; // The BMP header is done that way
 
 	// Create a buffer
-	data = new unsigned char[imageSize];
+	data = new unsigned char [imageSize];
 
 	// Read the actual data from the file into the buffer
-	fread(data, 1, imageSize, file);
+	fread(data,1,imageSize,file);
 
 	// Everything is in memory now, the file can be closed.
-	fclose(file);
+	fclose (file);
 
 	// Create one OpenGL texture
 	GLuint texture;
@@ -109,11 +97,10 @@ GLuint Model::build_texture(std::string path) {
 	glBindTexture(GL_TEXTURE_2D, texture);
 
 	// Give the image to OpenGL
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR,
-			GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
 
 	// OpenGL has now copied the data. Free our own version
-	delete[] data;
+	delete [] data;
 
 	// Poor filtering, or ...
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -123,44 +110,39 @@ GLuint Model::build_texture(std::string path) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-			GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	// ... which requires mipmaps. Generate them automatically.
-	glGenerateMipmap (GL_TEXTURE_2D);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	// Return the ID of the texture we just created
 	return texture;
 }
 
 GLuint Model::build_ebo() {
-	GLuint ebo;
-	glGenBuffers(1, &ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-			model_indices.size() * sizeof(unsigned int), &model_indices[0],
-			GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	 GLuint ebo;
+	 glGenBuffers(1, &ebo);
+	 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	 glBufferData(GL_ELEMENT_ARRAY_BUFFER, model_indices.size() * sizeof(unsigned int), &model_indices[0], GL_STATIC_DRAW);
+	 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	return ebo;
+	 return ebo;
 }
 
 GLuint Model::build_vbo() {
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, model_verts.size() * sizeof(glm::vec3),
-			&model_verts[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, model_verts.size() * sizeof(glm::vec3), &model_verts[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	return vbo;
+    return vbo;
 }
 
 GLuint Model::build_uvbo() {
 	GLuint uvbo;
 	glGenBuffers(1, &uvbo);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbo);
-	glBufferData(GL_ARRAY_BUFFER, model_uvs.size() * sizeof(glm::vec2),
-			&model_uvs[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, model_uvs.size() * sizeof(glm::vec2), &model_uvs[0], GL_STATIC_DRAW);
 
 	return uvbo;
 }
@@ -169,8 +151,7 @@ GLuint Model::build_nbo() {
 	GLuint nbo;
 	glGenBuffers(1, &nbo);
 	glBindBuffer(GL_ARRAY_BUFFER, nbo);
-	glBufferData(GL_ARRAY_BUFFER, model_normals.size() * sizeof(glm::vec3),
-			&model_normals[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, model_normals.size() * sizeof(glm::vec3), &model_normals[0], GL_STATIC_DRAW);
 
 	return nbo;
 }
@@ -178,8 +159,8 @@ GLuint Model::build_nbo() {
 cube_bound Model::get_bounds() {
 	cube_bound b;
 	bool flag = true;
-	for (auto t : model_verts) {
-		if (flag) {
+	for(auto t : model_verts){
+		if(flag) {
 			b.x1 = t.x;
 			b.x2 = t.x;
 			b.y1 = t.y;
@@ -189,15 +170,12 @@ cube_bound Model::get_bounds() {
 			flag = false;
 		}
 
-		if (t.x > b.x1)
-			b.x1 = t.x;
-		else if (t.x < b.x2) b.x2 = t.x;
-		if (t.y > b.y1)
-			b.y1 = t.y;
-		else if (t.y < b.y2) b.y2 = t.y;
-		if (t.z > b.z1)
-			b.z1 = t.z;
-		else if (t.z < b.z2) b.z2 = t.z;
+		if(t.x > b.x1) b.x1 = t.x;
+		else if(t.x < b.x2) b.x2 = t.x;
+		if(t.y > b.y1) b.y1 = t.y;
+		else if(t.y < b.y2) b.y2 = t.y;
+		if(t.z > b.z1) b.z1 = t.z;
+		else if(t.z < b.z2) b.z2 = t.z;
 	}
 	return b;
 
--- src/Core/WorldObject.cpp
+++ src/Core/WorldObject.cpp
@@ -1,75 +1,75 @@
 #include "WorldObject.h"
 #include "Libraries.h"
 
-float WorldObject::get_scale_x() {
+float WorldObject::get_scale_x(){
 	return scale_x;
 }
 
-float WorldObject::get_scale_y() {
+float WorldObject::get_scale_y(){
 	return scale_y;
 }
 
-float WorldObject::get_scale_z() {
+float WorldObject::get_scale_z(){
 	return scale_z;
 }
 
-float WorldObject::get_pos_x() {
+float WorldObject::get_pos_x(){
 	return pos_x;
 }
 
-float WorldObject::get_pos_y() {
+float WorldObject::get_pos_y(){
 	return pos_y;
 }
 
-float WorldObject::get_pos_z() {
+float WorldObject::get_pos_z(){
 	return pos_z;
 }
 
-float WorldObject::get_yaw() {
+float WorldObject::get_yaw(){
 	return pos_yaw;
 }
 
-float WorldObject::get_pitch() {
+float WorldObject::get_pitch(){
 	return pos_pitch;
 }
 
-float WorldObject::get_roll() {
+float WorldObject::get_roll(){
 	return pos_roll;
 }
 
-void WorldObject::set_scale_x(float n) {
+void WorldObject::set_scale_x(float n){
 	scale_x = n;
 }
 
-void WorldObject::set_scale_y(float n) {
+void WorldObject::set_scale_y(float n){
 	scale_y = n;
 }
 
-void WorldObject::set_scale_z(float n) {
+void WorldObject::set_scale_z(float n){
 	scale_z = n;
 }
 
-void WorldObject::set_pos_x(float n) {
+void WorldObject::set_pos_x(float n){
 	pos_x = n;
 }
 
-void WorldObject::set_pos_y(float n) {
+void WorldObject::set_pos_y(float n){
 	pos_y = n;
 }
 
-void WorldObject::set_pos_z(float n) {
+void WorldObject::set_pos_z(float n){
 	pos_z = n;
 }
 
-void WorldObject::set_pos_yaw(float n) {
+void WorldObject::set_pos_yaw(float n){
 	pos_yaw = n;
 }
 
-void WorldObject::set_pos_pitch(float n) {
+void WorldObject::set_pos_pitch(float n){
 	pos_pitch = n;
 }
 
-void WorldObject::set_pos_roll(float n) {
+void WorldObject::set_pos_roll(float n){
 	pos_roll = n;
 }
 
@@ -90,14 +90,13 @@ void WorldObject::build_rot(float _rx, float _ry, float _rz) {
 	mat_rot = glm::toMat4(quaternion);
 }
 
-void WorldObject::update_color(int a) {
+void WorldObject:: update_color(int a) {
 	glColor3f(colors[a][0], colors[a][1], colors[a][2]);
 }
 
-void WorldObject::update_verts(std::vector<glm::vec3> &_v,
-		std::vector<glm::vec3> &v) {
+void WorldObject::update_verts(std::vector<glm::vec3> &_v, std::vector<glm::vec3> &v){
 	glm::mat4 _mat_transform = mat_translate * mat_scale;
-	for (unsigned int i = 0; i < v.size(); i++) {
+	for(unsigned int i = 0; i < v.size(); i++) {
 		_v.at(i) = glm::vec3(_mat_transform * glm::vec4(v.at(i), 1.0f));
 	}
 }
@@ -111,14 +110,12 @@ void WorldObject::update(float delta, GLuint programID) {
 	build_bounds();
 	update_verts(bb_verts, bbverts);
 
-	if (animated) {
+	if(animated) {
 		delta_arg += delta;
-		if (delta_arg >= 0.25f) {
+		if(delta_arg >= 0.25f) {
 			delta_arg -= 0.25f;
-			if (current_model == signed(model.size() - 1))
-				current_model = 0;
-			else
-				current_model++;
+			if(current_model == signed(model.size() - 1)) current_model = 0;
+			else current_model++;
 		}
 	}
 
@@ -126,69 +123,65 @@ void WorldObject::update(float delta, GLuint programID) {
 
 void WorldObject::display(float delta, GLuint programID) {
 
-	if (draw_bb || !use_model) {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		GLuint matrixAttributePosition = glGetUniformLocation(programID,
-				"mat_model_view");
-		glm::mat4 _mat_transform = mat_translate * mat_scale;
-		glUniformMatrix4fv(matrixAttributePosition, 1, GL_FALSE,
-				&_mat_transform[0][0]);
+		if(draw_bb || !use_model) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			GLuint matrixAttributePosition = glGetUniformLocation(programID, "mat_model_view");
+			glm::mat4 _mat_transform = mat_translate * mat_scale;
+			glUniformMatrix4fv(matrixAttributePosition, 1, GL_FALSE, &_mat_transform[0][0]);
 
-		glBindBuffer(GL_ARRAY_BUFFER, debug_vboId);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, debug_eboId);
+		    glBindBuffer(GL_ARRAY_BUFFER, debug_vboId);
+		    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, debug_eboId);
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-		glDisableVertexAttribArray(0);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			glDisableVertexAttribArray(0);
 
-	}
+		}
 
-	if (use_model) {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		GLuint textureID = glGetUniformLocation(programID, "sampler");
-		GLuint matrixAttributePosition = glGetUniformLocation(programID,
-				"mat_model_view");
-		glUniformMatrix4fv(matrixAttributePosition, 1, GL_FALSE,
-				&mat_transform[0][0]);
+		if(use_model){
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			GLuint textureID  = glGetUniformLocation(programID, "sampler");
+			GLuint matrixAttributePosition = glGetUniformLocation(programID, "mat_model_view");
+			glUniformMatrix4fv(matrixAttributePosition, 1, GL_FALSE, &mat_transform[0][0]);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, model.at(current_model)->textureId);
-		glUniform1i(textureID, 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, model.at(current_model)->textureId);
+			glUniform1i(textureID, 0);
 
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model.at(current_model)->eboId);
 
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glEnableVertexAttribArray(2);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model.at(current_model)->eboId);
 
-		glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->vboId);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+			glEnableVertexAttribArray(2);
 
-		glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->uvboId);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+			glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->vboId);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->nboId);
-		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->uvboId);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDrawElements(GL_TRIANGLES,
-				model.at(current_model)->model_indices.size(), GL_UNSIGNED_INT,
-				0);
+			glBindBuffer(GL_ARRAY_BUFFER, model.at(current_model)->nboId);
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-		glDisableVertexAttribArray(2);
+			glDrawElements(GL_TRIANGLES, model.at(current_model)->model_indices.size(), GL_UNSIGNED_INT, 0);
 
-	}
+			glDisableVertexAttribArray(0);
+			glDisableVertexAttribArray(1);
+			glDisableVertexAttribArray(2);
+
+		}
+
 
 }
 
 cube_bound WorldObject::get_bounds() {
 	cube_bound b;
 	bool flag = true;
-	for (auto t : bb_verts) {
-		if (flag) {
+	for(auto t : bb_verts){
+		if(flag) {
 			b.x1 = t.x;
 			b.x2 = t.x;
 			b.y1 = t.y;
@@ -198,15 +191,12 @@ cube_bound WorldObject::get_bounds() {
 			flag = false;
 		}
 
-		if (t.x > b.x1)
-			b.x1 = t.x;
-		else if (t.x < b.x2) b.x2 = t.x;
-		if (t.y > b.y1)
-			b.y1 = t.y;
-		else if (t.y < b.y2) b.y2 = t.y;
-		if (t.z > b.z1)
-			b.z1 = t.z;
-		else if (t.z < b.z2) b.z2 = t.z;
+		if(t.x > b.x1) b.x1 = t.x;
+		else if(t.x < b.x2) b.x2 = t.x;
+		if(t.y > b.y1) b.y1 = t.y;
+		else if(t.y < b.y2) b.y2 = t.y;
+		if(t.z > b.z1) b.z1 = t.z;
+		else if(t.z < b.z2) b.z2 = t.z;
 	}
 	return b;
 
